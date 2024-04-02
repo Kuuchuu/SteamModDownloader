893a9891523c882e7785e67b8ec1018dd80aeaf8
Blase <Blase@Cat.Degree>
2024-04-01 21:56:16 -0500

    parse args in __main__

--- __main__.py
+++ __main__.py
@@ -1,8 +1,19 @@
 from scripts.init import start
-from sys import argv
+import argparse
 
-Repo_Owner = argv[1] if len(argv) > 1 else None
-Repo_Name = argv[2] if len(argv) > 1 else None
+def main():
+    parser = argparse.ArgumentParser(description="Launch SMD")
+    parser.add_argument("Repo_Owner", help="Repository Owner", nargs='?', default="Kuuchuu")
+    parser.add_argument("Repo_Name", help="Repository Name", nargs='?', default="SteamModDownloader")
+    parser.add_argument("-c", "--config", type=json.loads, help="Configuration in JSON format.")
+    parser.add_argument("-f", "--configFile", help="Path to the configuration file.")
+    parser.add_argument("-g", "--game", help="Game's Steam ID.")
+    parser.add_argument("-m", "--mod", help="Mod ID numbers or URLs.")
+    parser.add_argument("-p", "--pack", help="Pack ID numbers or URLs.")
+    parser.add_argument("-o", "--outputDir", help="Path to the mod download output directory.")
+    
+    args = parser.parse_args()
+    start(args.Repo_Owner, args.Repo_Name, vars(args))
 
 if __name__=="__main__":
-    start(Repo_Owner, Repo_Name)
+    main()
